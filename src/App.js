0b429a376fc72e1f626b8c9959753a97a4be5a5b
Ariel Lorenzo <ariellorenzo08@gmail.com>
2019-11-07 08:50:03 -0500

    All buttons press on their own and display the notes being played on the bottom text.

--- src/App.js
+++ src/App.js
@@ -1,78 +1,12 @@
 import React from 'react';
+import useState from 'react';
 import './App.css';
 
-const BlackNote = props => {
-  let up = props.x + "," + props.y + " " +
-    props.x + "," + (props.y+150) + " " +
-    (props.x+10) + "," + (props.y+160) + " " +
-    (props.x+70) + "," + (props.y+160) + " " +
-    (props.x+80) + "," + (props.y+150) + " " +
-    (props.x+80) + "," + props.y;
-  
-  let down = props.x + "," + props.y + " " +
-    props.x + "," + (props.y+160) + " " +
-    (props.x+10) + "," + (props.y+170) + " " +
-    (props.x+70) + "," + (props.y+170) + " " +
-    (props.x+80) + "," + (props.y+160) + " " +
-    (props.x+80) + "," + props.y;
-
-    let played;
-    if(props.pressed) played = down;
-    else played = up;
-
-  return (
-    <g id={props.id}>
-      <polygon
-        className="App-black-note-gray"
-        points={down} />
-      <polygon id={props.id.slice(4, props.id.length)}
-        className="App-black-note"
-        onMouseDown={props.onMouseDown}
-        onMouseUp={props.onMouseUp}
-        points={played} />
-    </g>
-  )
-}
-
-const Note = props => {
-  let up = props.x + "," + props.y + " " +
-    props.x + "," + (props.y+300) + " " +
-    (props.x+10) + "," + (props.y+310) + " " +
-    (props.x+90) + "," + (props.y+310) + " " +
-    (props.x+100) + "," + (props.y+300) + " " +
-    (props.x+100) + "," + props.y;
-  
-  let down = props.x + "," + props.y + " " +
-    props.x + "," + (props.y+310) + " " +
-    (props.x+10) + "," + (props.y+320) + " " +
-    (props.x+90) + "," + (props.y+320) + " " +
-    (props.x+100) + "," + (props.y+310) + " " +
-    (props.x+100) + "," + props.y;
-
-  let played;
-  if(props.pressed) played = down;
-  else played = up;
-  
-  return (
-    <g id={props.id}>
-      <polygon
-        className="App-white-note-gray"
-        points={down} />
-      <polygon id={props.id.slice(4, props.id.length)}
-        className="App-white-note"
-        onMouseDown={props.onMouseDown}
-        onMouseUp={props.onMouseUp}
-        points={played} />
-    </g>
-  );
-};
-
-class App extends React.Component {
+class BlackNote extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      noteName: "Played note will appear here",
-      played: false
+      pressed: false
     };
 
     this.playNote = this.playNote.bind(this);
@@ -80,18 +14,114 @@ class App extends React.Component {
   }
 
   playNote(e) {
-    this.setState({
-      noteName: e.target.id.replace('Sharp','#').replace('Flat','b')
-    });
-    this.setState({
-      played: true
-    });
+    this.setState({pressed: true});
+    this.props.noteName(e.target.id.replace('Sharp','#').replace('Flat','b'));
   }
 
   stopNote(e) {
-    this.setState({
-      played: false
-    });
+    this.setState({pressed: false});
+  }
+
+  render() {
+    let up = this.props.x + "," + this.props.y + " " +
+      this.props.x + "," + (this.props.y+150) + " " +
+      (this.props.x+10) + "," + (this.props.y+160) + " " +
+      (this.props.x+70) + "," + (this.props.y+160) + " " +
+      (this.props.x+80) + "," + (this.props.y+150) + " " +
+      (this.props.x+80) + "," + this.props.y;
+    
+    let down = this.props.x + "," + this.props.y + " " +
+      this.props.x + "," + (this.props.y+160) + " " +
+      (this.props.x+10) + "," + (this.props.y+170) + " " +
+      (this.props.x+70) + "," + (this.props.y+170) + " " +
+      (this.props.x+80) + "," + (this.props.y+160) + " " +
+      (this.props.x+80) + "," + this.props.y;
+
+    let played;
+    if(this.state.pressed) played = down;
+    else played = up;
+
+    return (
+      <g id={this.props.id}>
+        <polygon
+          className="App-black-note-gray"
+          points={down} />
+        <polygon id={this.props.id.slice(4, this.props.id.length)}
+          className="App-black-note"
+          onMouseDown={this.playNote}
+          onMouseUp={this.stopNote}
+          points={played} />
+      </g>
+    );
+  }
+}
+
+class Note extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pressed: false
+    };
+
+    this.playNote = this.playNote.bind(this);
+    this.stopNote = this.stopNote.bind(this);
+  }
+
+  playNote(e) {
+    this.setState({pressed: true});
+    this.props.noteName(e.target.id.replace('Sharp','#').replace('Flat','b'));
+  }
+
+  stopNote(e) {
+    this.setState({pressed: false});
+  }
+
+  render() {
+    let up = this.props.x + "," + this.props.y + " " +
+      this.props.x + "," + (this.props.y+300) + " " +
+      (this.props.x+10) + "," + (this.props.y+310) + " " +
+      (this.props.x+90) + "," + (this.props.y+310) + " " +
+      (this.props.x+100) + "," + (this.props.y+300) + " " +
+      (this.props.x+100) + "," + this.props.y;
+    
+    let down = this.props.x + "," + this.props.y + " " +
+      this.props.x + "," + (this.props.y+310) + " " +
+      (this.props.x+10) + "," + (this.props.y+320) + " " +
+      (this.props.x+90) + "," + (this.props.y+320) + " " +
+      (this.props.x+100) + "," + (this.props.y+310) + " " +
+      (this.props.x+100) + "," + this.props.y;
+
+    let played;
+    if(this.state.pressed) played = down;
+    else played = up;
+    
+    return (
+      <g id={this.props.id}>
+        <polygon
+          className="App-white-note-gray"
+          points={down} />
+        <polygon id={this.props.id.slice(4, this.props.id.length)}
+          className="App-white-note"
+          onMouseDown={this.playNote}
+          onMouseUp={this.stopNote}
+          points={played} />
+      </g>
+    );
+  }
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteName: "Played note will appear here"
+    };
+
+    this.changeNoteName = this.changeNoteName.bind(this);
+  }
+
+  changeNoteName(note) {
+    this.setState({noteName: note});
   }
 
   render() {
@@ -100,68 +130,20 @@ class App extends React.Component {
         <header className="App-header">
           <svg className="App-logo">
             {/* White Notes */}
-            <Note id="noteC"
-              x={0} y={0}
-              pressed={this.state.played}
-              onMouseDown={this.playNote}
-              onMouseUp={this.stopNote} />
-            <Note id="noteD"
-              x={100} y={0}
-              pressed={this.state.played}
-              onMouseDown={this.playNote}
-              onMouseUp={this.stopNote} />
-            <Note id="noteE"
-              x={200} y={0}
-              pressed={this.state.played}
-              onMouseDown={this.playNote}
-              onMouseUp={this.stopNote} />
-            <Note id="noteF"
-              x={300} y={0}
-              pressed={this.state.played}
-              onMouseDown={this.playNote}
-              onMouseUp={this.stopNote} />
-            <Note id="noteG"
-              x={400} y={0}
-              pressed={this.state.played}
-              onMouseDown={this.playNote}
-              onMouseUp={this.stopNote} />
-            <Note id="noteA"
-              x={500} y={0}
-              pressed={this.state.played}
-              onMouseDown={this.playNote}
-              onMouseUp={this.stopNote} />
-            <Note id="noteB"
-              x={600} y={0}
-              pressed={this.state.played}
-              onMouseDown={this.playNote}
-              onMouseUp={this.stopNote} />
+            <Note id="noteC" noteName={this.changeNoteName} x={0} y={0} />
+            <Note id="noteD" noteName={this.changeNoteName} x={100} y={0} />
+            <Note id="noteE" noteName={this.changeNoteName} x={200} y={0} />
+            <Note id="noteF" noteName={this.changeNoteName} x={300} y={0} />
+            <Note id="noteG" noteName={this.changeNoteName} x={400} y={0} />
+            <Note id="noteA" noteName={this.changeNoteName} x={500} y={0} />
+            <Note id="noteB" noteName={this.changeNoteName} x={600} y={0} />
 
             {/* Black Notes */}
-            <BlackNote id="noteCSharpDFlat"
-              x={60} y={0}
-              pressed={this.state.played}
-              onMouseDown={this.playNote}
-              onMouseUp={this.stopNote} />
-            <BlackNote id="noteDSharpEFlat"
-              x={160} y={0}
-              pressed={this.state.played}
-              onMouseDown={this.playNote}
-              onMouseUp={this.stopNote} />
-            <BlackNote id="noteFSharpGFlat"
-              x={360} y={0}
-              pressed={this.state.played}
-              onMouseDown={this.playNote}
-              onMouseUp={this.stopNote} />
-            <BlackNote id="noteGSharpAFlat"
-              x={460} y={0}
-              pressed={this.state.played}
-              onMouseDown={this.playNote}
-              onMouseUp={this.stopNote} />
-            <BlackNote id="noteASharpBFlat"
-              x={560} y={0}
-              pressed={this.state.played}
-              onMouseDown={this.playNote}
-              onMouseUp={this.stopNote} />
+            <BlackNote id="noteCSharpDFlat" noteName={this.changeNoteName} x={60} y={0} />
+            <BlackNote id="noteDSharpEFlat" noteName={this.changeNoteName} x={160} y={0} />
+            <BlackNote id="noteFSharpGFlat" noteName={this.changeNoteName} x={360} y={0} />
+            <BlackNote id="noteGSharpAFlat" noteName={this.changeNoteName} x={460} y={0} />
+            <BlackNote id="noteASharpBFlat" noteName={this.changeNoteName} x={560} y={0} />
           </svg>
           <p className="App-note-name">
             {this.state.noteName}
